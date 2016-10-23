066c6b79bc9472c18d376b7adc65639bc93cbac8
droooney <d.vor7@mail.ru>
2016-10-24 02:13:42 +0500

    Replace html with pug.

--- server.js
+++ server.js
@@ -4,7 +4,12 @@ const express = require('express');
 const port = 9999;
 const app = express();
 
+app.set('views', path.resolve('./'));
+app.set('view engine', 'pug');
 app.use('/', express.static(path.resolve('./')));
+app.use('/', (req, res) => {
+  res.render('index');
+});
 
 app.listen(port, () => {
   console.log(`Listening on ${ port }...`);
